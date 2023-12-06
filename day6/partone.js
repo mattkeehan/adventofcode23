8fe6a6054f4ec51372fd82df6d154deb57a1d8fb
Matt Keehan <matt.keehan.ext@bbc.co.uk>
2023-12-06 17:46:41 +0000

    finds day 6 part 1 solution

---
+++ day6/partone.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const getTimes = (data) =>
+  data.map((d) =>
+    d.includes("Time:")
+      ? d
+          .substr("Time:".length, d.length)
+          .trim()
+          .split(" ")
+          .filter((subArray) => subArray.length > 0)
+      : d
+          .substr("Distance:".length, d.length)
+          .trim()
+          .split(" ")
+          .filter((subArray) => subArray.length > 0)
+  );
+
+const findNumberPairs = (arr) => {
+  return arr.map((target) => {
+    const pairs = [];
+    for (let i = 0; i <= target; i++) pairs.push(i * (target - i));
+    return pairs;
+  });
+};
+
+const getResult = (data) => {
+  const [times, distances] = getTimes(data);
+  const nmbrp = findNumberPairs(times);
+
+  return distances.map((d, idx) => {
+    let count = 0;
+    nmbrp[idx].forEach((n) => {
+      if (n > d) count++;
+    });
+    return count;
+  }).reduce((acc, b) => b * acc);
+};
+
+try {
+  console.log("hello advent of code day 6");
+  const data = fs.readFileSync("./input.txt", "utf8").split("\n");
+  const result = getResult(data);
+  console.log(result);
+} catch (err) {
+  console.error(err);
+}
