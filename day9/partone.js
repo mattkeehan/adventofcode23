14f804a323271e3cfed92983c577ddf1ec21c409
Matt Keehan <matt.keehan.ext@bbc.co.uk>
2023-12-09 20:55:06 +0000

    day 9 part one

---
+++ day9/partone.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const allSame = (nums) => nums.every((n) => n === nums[0]);
+
+const findDifferences = (lineAsArr) => {
+  const diffs = [];
+
+  for (let i = 0; i < lineAsArr.length - 1; i++) {
+    diffs.push(lineAsArr[i + 1] - lineAsArr[i]);
+  }
+  return diffs;
+};
+
+const nextDiffValue = (lineAsArr) => {
+  if (allSame(lineAsArr)) return lineAsArr[0];
+
+  const diffs = findDifferences(lineAsArr);
+  const nextDiff = nextDiffValue(diffs);
+  return lineAsArr[lineAsArr.length - 1] + nextDiff;
+};
+
+const getResult = (lines) => {
+  const lineAsArr = lines.map((line) => line.split(" ").map((n) => Number(n)));
+  const lineTotals = lineAsArr.map(nextDiffValue);
+  return lineTotals.reduce((total, num) => total + num, 0);
+};
+
+try {
+  console.log("hello advent of code day 9");
+  const data = fs.readFileSync("./input.txt", "utf8").split("\n");
+  console.log(getResult(data));
+} catch (err) {
+  console.error(err);
+}
